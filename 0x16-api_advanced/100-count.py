fffa33360c0fcefe18c4042a42f67898327a2f46
Beshoy.Ayad <Beshoy.Ayad@gmail.com>
2023-11-08 10:40:01 -0800

    update 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,38 +3,50 @@
 Python script that, using this REST API, for a given subreddit,
 returns all its hot posts for a given subreddit.
 """
+
 import requests
 
+def count_words(subreddit, word_list, after=None, results=None):
+    if results is None:
+        results = {}
 
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    if count_dict is None:
-        count_dict = {}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        after = data['data']['after']
-        for post in posts:
-            title = post['data']['title'].lower()
-            for word in word_list:
-                if word.lower() in title:
-                    index = title.index(word.lower())
-                    if (index == 0 or not title[
-                        index-1].isalpha()) and (index+len(word) == len(
-                            title) or not title[index+len(word)].isalpha()):
-                        if word.lower() in count_dict:
-                            count_dict[word.lower()] += 1
-                        else:
-                            count_dict[word.lower()] = 1
-        if after:
-            return count_words(subreddit, word_list, after, count_dict)
-        else:
-            sorted_counts = sorted(
-                count_dict.items(), key=lambda x: (-x[1], x[0]))
-            for count in sorted_counts:
-                print(f"{count[0]}: {count[1]}")
+    headers = {'User-Agent': 'Mozilla/5.0'}  # Provide a user agent to prevent issues with Reddit API
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after or ""}'
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(f"Error: Unable to fetch data from Reddit. Status code: {response.status_code}")
+        return
+
+    data = response.json().get('data', {})
+    articles = data.get('children', [])
+
+    for article in articles:
+        title = article['data']['title'].lower()
+        for word in word_list:
+            if word.lower() in title:
+                results[word] = results.get(word, 0) + title.count(word.lower())
+
+    after = data.get('after')
+    if after:
+        return count_words(subreddit, word_list, after, results)
     else:
-        print(None)
+        print_results(results)
+
+def print_results(results):
+    sorted_results = sorted(results.items(), key=lambda item: (-item[1], item[0].lower()))
+    for word, count in sorted_results:
+        print(f"{word}: {count}")
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Example: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        subreddit = sys.argv[1]
+        word_list = sys.argv[2].split()
+        count_words(subreddit, word_list)
+
